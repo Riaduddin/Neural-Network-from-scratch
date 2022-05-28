582cc069192db56075df4f96ae775642d26e002f
Riaduddin <rrriaduddin@gmail.com>
2022-05-28 10:31:06 +0600

    backpropation in layer & relu

--- Layer.py
+++ Layer.py
@@ -6,6 +6,18 @@ class Dense:
         self.weights=np.random.randn(n_inputs,n_neurons)
         self.biases=np.zeros((1,n_neurons))
     
+    #forward pass
     def forward(self,inputs):
         #calculating the z=wx+b
-        self.output=np.dot(inputs,self.weights)+sefl.biases 
+        self.output=np.dot(inputs,self.weights)+self.biases
+        #remember input variables
+        self.inputs=inputs
+    
+    #backward pass
+    def backward(self,dvalues):
+        #Gradient on parameters
+        self.dweights=np.dot(self.inputs.T,dvalues)
+        self.dbiases=np.sum(dvalues,axis=0,keepdims=True)
+
+        #Gradient on inputs
+        self.dinputs=np.dot(dvalues,self.weights.T)
--- activation.py
+++ activation.py
@@ -2,7 +2,15 @@ import numpy as np
 
 class ReLU:
     def forward(self,inputs):
+        #remember input variables
+        self.inputs=inputs
         self.output=np.maximum(0,inputs)
+    
+    def backward(self,dvalues):
+
+        self.dinputs=dvalues.copy()
+        #zero gradient on negative inputs
+        self.dinputs[self.inputs<=0]=0
 
 class Softmax:
     def forward(self,inputs):
