481cbe67167ff2a7fd0354812cf1f55496825840
Riaduddin <rrriaduddin@gmail.com>
2022-06-09 12:16:36 +0600

    testing all function together

--- loss.py
+++ loss.py
@@ -1,36 +1,4 @@
 import numpy as np
-class Loss:
-    def calculate(self,output,y):
-        sample_losses=self.forward(output,y)
-
-        mean_losses=np.mean(sample_losses)
-
-        return mean_losses
-
-
-
-class Categorical_loss_entropy(Loss):
-    def forward(self,y_pred,y_true):
-        samples=len(y_pred)
-
-        y_pred_clipped=np.clip(y_pred,1e-7,1-1e-7)
-
-        if len(y_true)==1:
-            correct_confidences=y_pred_clipped[range(samples),y_true]
-
-        elif len(y_true)==2:
-            correct_confidences=np.sum(y_pred_clipped*y_true,axis=1)
-        
-        negative_log_likelihood=-np.log(correct_confidences)
-
-        return negative_log_likelihood
-    def backward(self,dvalues,y_true):
-        samples=len(dvalues)
-        labels=len(dvalues[0])
-        if len(y_true.shape)==1:
-            y_true=np.eye(labels)[y_true]
-        self.dinputs=-y_true/dvalues
-        self.dinputs=self.dinputs/samples
 
 class Activation_Softmax:
     def forward(self,inputs):
@@ -48,6 +16,39 @@ class Activation_Softmax:
             jacobian_matrix=np.diagflat(single_output)-np.dot(single_output,single_output.T)
             self.dinputs[index]=np.dot(jacobian_matrix,single_dvalues)
 
+class Loss:
+    def calculate(self,output,y):
+        sample_losses=self.forward(output,y)
+
+        mean_losses=np.mean(sample_losses)
+
+        return mean_losses
+
+
+
+class Categorical_loss_entropy(Loss):
+
+    def forward(self,y_pred,y_true):
+        samples=len(y_pred)
+
+        y_pred_clipped=np.clip(y_pred,1e-7,1-1e-7)
+
+        if len(y_true.shape)==1:
+            correct_confidences=y_pred_clipped[range(samples),y_true]
+
+        elif len(y_true.shape)==2:
+            correct_confidences=np.sum(y_pred_clipped*y_true,axis=1)
+        
+        negative_log_likelihood=-np.log(correct_confidences)
+
+        return negative_log_likelihood
+    def backward(self,dvalues,y_true):
+        samples=len(dvalues)
+        labels=len(dvalues[0])
+        if len(y_true.shape)==1:
+            y_true=np.eye(labels)[y_true]
+        self.dinputs=-y_true/dvalues
+        self.dinputs=self.dinputs/samples
 
 
 class Activation_Softmax_Loss_Categoricalcrossentropy():
--- testing.py
+++ testing.py
@@ -1,22 +1,63 @@
 from Layer import Dense
-from activation import ReLU,Softmax
+from activation import ReLU
+from loss import Activation_Softmax_Loss_Categoricalcrossentropy
+
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
 # x=Dense(2,3)
 # print(x.weights)
 # print(x.biases)
-
-a=[1,2,-3,5,-3,45,0]
-b=[[1,2,-3,5,-3,45,0]]
-
-softmax_output=[[.2,.3,.7],[.3,.2,.5],[.2,.7,.3]]
-
-
-
-
+# a=[1,2,-3,5,-3,45,0]
+# b=[[1,2,-3,5,-3,45,0]]
+# softmax_output=[[.2,.3,.7],[.3,.2,.5],[.2,.7,.3]]
 # x=ReLU()
 # x.forward(a)
 # y=Softmax()
 # y.forward(b)
-
 # print(x.output)
 # print(y.output)
+
+X,y=spiral_data(samples=100,classes=3)
+
+dense1=Dense(2,3)
+activation1=ReLU()
+dense2=Dense(3,4)
+activation2=ReLU()
+dense3=Dense(4,3)
+
+loss_activation=Activation_Softmax_Loss_Categoricalcrossentropy()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+dense3.forward(activation2.output)
+print(dense1.weights)
+
+
+loss=loss_activation.forward(dense3.output,y)
+#print(loss_activation.output[:5])
+print('loss:',loss)
+
+predictions=np.argmax(loss_activation.output,axis=1)
+if len(y.shape)==2:
+    y=np.argmax(y,axis=1)
+accuracy=np.mean(predictions==y)
+
+print('acc:',accuracy)
+
+loss_activation.backward(loss_activation.output,y)
+dense3.backward(loss_activation.dinputs)
+activation2.backward(dense3.dinputs)
+dense2.backward(activation2.dinputs)
+activation1.backward(dense2.dinputs)
+dense1.backward(activation1.dinputs)
+
+print(dense1.dweights)
+print(dense1.dbiases)
+print(dense2.dweights)
+print(dense2.dbiases)
+print(dense3.dweights)
+print(dense3.dbiases)
